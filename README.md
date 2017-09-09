643265e39519d2922b9a86d655f58556c289d087
edgargmc <edgargmc@gmail.com>
2017-09-09 12:09:38 -0300

    add readme

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # opra_task
+
+Clone Repo.
+
+Config Data Base iin file : \application\config\database.php
+
+create data base with the file : opratel.sql
+
--- opratel.sql
+++ opratel.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 08-09-2017 a las 13:14:20
+-- Tiempo de generación: 09-09-2017 a las 15:07:15
 -- Versión del servidor: 5.7.11
 -- Versión de PHP: 7.0.4
 
@@ -51,7 +51,7 @@ CREATE TABLE `tasks` (
   `id_status` int(11) NOT NULL,
   `author` varchar(50) COLLATE utf8_spanish_ci NOT NULL,
   `description` varchar(250) COLLATE utf8_spanish_ci NOT NULL,
-  `date_create` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP
+  `date_create` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_spanish_ci;
 
 --
@@ -59,10 +59,9 @@ CREATE TABLE `tasks` (
 --
 
 INSERT INTO `tasks` (`id`, `id_status`, `author`, `description`, `date_create`) VALUES
-(6, 1, 'Juan', 'task 1 ', '2016-09-09 02:10:05'),
-(7, 1, 'Karla', 'task 2', '2017-09-08 02:19:50'),
-(8, 3, 'Cristina', 'task 3', '2017-08-08 02:41:25'),
-(9, 1, 'Omar', 'task 4 ', '2017-09-08 13:13:40');
+(1, 3, 'Edgar', 'task 0', '2017-09-08 18:06:42'),
+(2, 1, 'Omar g', 'task 1 yesterday', '2017-09-07 18:07:16'),
+(3, 1, 'Cristina', 'task 2 yestaday 2', '2017-09-07 18:08:09');
 
 --
 -- Índices para tablas volcadas
@@ -89,7 +88,7 @@ ALTER TABLE `tasks`
 -- AUTO_INCREMENT de la tabla `tasks`
 --
 ALTER TABLE `tasks`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 --
 -- Restricciones para tablas volcadas
 --
